822d9ef6410d411be9350fb28a147348e538a901
ku <zaiku.zhuang@melot.cn>
2017-10-11 16:29:22 +0800

    do not guide when first open

--- app/src/main/java/tv/niuwa/live/SplashActivity.java
+++ app/src/main/java/tv/niuwa/live/SplashActivity.java
@@ -25,60 +25,63 @@ public class SplashActivity extends BaseSplashActivity {
     boolean firstOpen;
     @Bind(R.id.lauch_screen)
     ImageView mLauchScreen;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        isFirstOpen();
-        if(!firstOpen){
-            Api.getLaunchScreen(this, new JSONObject(), new OnRequestDataListener() {
-                @Override
-                public void requestSuccess(int code, final JSONObject data) {
-                    Glide.with(SplashActivity.this).load(data.getString("info")).into(mLauchScreen);
-                    mLauchScreen.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View view) {
-                            if(null != data.getString("url") && null != data.getString("title")){
-                                Bundle temp = new Bundle();
-                                temp.putString("title",data.getString("title"));
-                                temp.putString("jump",data.getString("url"));
-                                openActivity(WebviewActivity.class,temp);
-                                SplashActivity.this.finish();
-                            }
+//        isFirstOpen();
+//        if (!firstOpen) {
+        Api.getLaunchScreen(this, new JSONObject(), new OnRequestDataListener() {
+            @Override
+            public void requestSuccess(int code, final JSONObject data) {
+                Glide.with(SplashActivity.this).load(data.getString("info")).into(mLauchScreen);
+                mLauchScreen.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (null != data.getString("url") && null != data.getString("title")) {
+                            Bundle temp = new Bundle();
+                            temp.putString("title", data.getString("title"));
+                            temp.putString("jump", data.getString("url"));
+                            openActivity(WebviewActivity.class, temp);
+                            SplashActivity.this.finish();
                         }
-                    });
-                }
-
-                @Override
-                public void requestFailure(int code, String msg) {
-
-                }
-            });
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    if (!destroy) {
-                        openActivity(MainActivity.class);
-                        SplashActivity.this.finish();
                     }
+                });
+            }
+
+            @Override
+            public void requestFailure(int code, String msg) {
+
+            }
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (!destroy) {
+                    openActivity(MainActivity.class);
+                    SplashActivity.this.finish();
                 }
-            }, 400);// TODO: 28/09/2017  
-        }
+            }
+        }, 400);// TODO: 28/09/2017
+//        }
 
 
     }
-    private void isFirstOpen(){
-        firstOpen = (boolean) SharePrefsUtils.get(SplashActivity.this, "system", "isFirstOpen", true);
-        if (firstOpen){
-            new Handler().postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    startActivity(new Intent(SplashActivity.this,FirstOpenActivity.class));
-                    finish();
-                    SharePrefsUtils.put(SplashActivity.this, "system", "isFirstOpen", false);
-                }
-            },3000);
-        }
-    }
+
+//    private void isFirstOpen() {
+//        firstOpen = (boolean) SharePrefsUtils.get(SplashActivity.this, "system", "isFirstOpen", true);
+//        if (firstOpen) {
+//            new Handler().postDelayed(new Runnable() {
+//                @Override
+//                public void run() {
+//                    startActivity(new Intent(SplashActivity.this, FirstOpenActivity.class));
+//                    finish();
+//                    SharePrefsUtils.put(SplashActivity.this, "system", "isFirstOpen", false);
+//                }
+//            }, 3000);
+//        }
+//    }
+
     @Override
     public int getLayoutResource() {
         return R.layout.activity_splash;
